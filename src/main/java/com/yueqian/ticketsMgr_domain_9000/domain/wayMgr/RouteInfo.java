3673462aca3ce5a936246b42328b96f52bb4bdb3
wwj <1428879014@qq.com>
2020-11-19 17:44:59 +0800

    修改线路模型

--- src/main/java/com/yueqian/ticketsMgr_domain_9000/domain/wayMgr/RouteInfo.java
+++ src/main/java/com/yueqian/ticketsMgr_domain_9000/domain/wayMgr/RouteInfo.java
@@ -2,43 +2,17 @@ package com.yueqian.ticketsMgr_domain_9000.domain.wayMgr;
 
 public class RouteInfo {
 	private Integer routeId;
-	private Integer startStation;
-	private Integer terminalStation;
+	private Station startStation;
+	private Station terminalStation;
 	private String routeName;
 	private Double price;
 	private Integer delMark;
-	private Station startS;
-	private Station terminalS;
 	
-	public Station getStartS() {
-		return startS;
-	}
-	public void setStartS(Station startS) {
-		this.startS = startS;
-	}
-	public Station getTerminalS() {
-		return terminalS;
-	}
-	public void setTerminalS(Station terminalS) {
-		this.terminalS = terminalS;
-	}
 	public Integer getRouteId() {
 		return routeId;
 	}
 	public void setRouteId(Integer routeId) {
 		this.routeId = routeId;
-	}
-	public Integer getStartStation() {
-		return startStation;
-	}
-	public void setStartStation(Integer startStation) {
-		this.startStation = startStation;
-	}
-	public Integer getTerminalStation() {
-		return terminalStation;
-	}
-	public void setTerminalStation(Integer terminalStation) {
-		this.terminalStation = terminalStation;
 	}
 	public String getRouteName() {
 		return routeName;
@@ -58,11 +32,25 @@ public class RouteInfo {
 	public void setDelMark(Integer delMark) {
 		this.delMark = delMark;
 	}
+	
+	public Station getStartStation() {
+		return startStation;
+	}
+	public void setStartStation(Station startStation) {
+		this.startStation = startStation;
+	}
+	public Station getTerminalStation() {
+		return terminalStation;
+	}
+	public void setTerminalStation(Station terminalStation) {
+		this.terminalStation = terminalStation;
+	}
 	@Override
 	public String toString() {
 		return "RouteInfo [routeId=" + routeId + ", startStation=" + startStation + ", terminalStation="
 				+ terminalStation + ", routeName=" + routeName + ", price=" + price + ", delMark=" + delMark + "]";
 	}
+	
 	
 	
 }
